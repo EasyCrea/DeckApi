b1b61853906def44592fac20f113f0b42cbb6d70
eliotpp1 <eliot.pouplier@gmail.com>
2024-12-16 16:32:08 +0100

    add localhost and easycrea online on allow cross origin

--- src/Controller/AuthorizationController.php
+++ src/Controller/AuthorizationController.php
@@ -15,7 +15,14 @@ class AuthorizationController extends Controller
      */
     public function options(): void
     {
-        header('Access-Control-Allow-Origin: http://localhost:5173');
+        $allowed_origins = [
+            'http://localhost:5173',
+            'https://easydeck.alwaysdata.net'
+        ];
+
+        if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+            header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        }
         header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With');
         header('Access-Control-Allow-Credentials: true');
